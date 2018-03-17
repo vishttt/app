6a7fc9de1a90ff51dc8b24efd5049b87e3b3899e
Larry Boltovskoi <larry.bolt@gmail.com>
2018-03-17 21:20:05 +0100

    Added sessions for express and socketio

--- server.js
+++ server.js
@@ -2,19 +2,36 @@
 require('dotenv').config()
 
 const { Client } = require('pg')
-const app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+const path = require('path')
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const port = process.env.PORT || 3000
+
+const session = require('express-session')({
+    secret: 'my-secret',
+        resave: true,
+        saveUninitialized: true
+    }
+)
+const sharedsession = require('express-socket.io-session')
+
+// use sessions for express
+app.use(session)
+
+// Share session with io sockets
+io.use(sharedsession(session))
 
 // connect to the PostgreSQL server
 const client = new Client({
     connectionString: process.env.DB_URI,
-    ssl : {
-        rejectUnauthorized : true
-    }
+    ssl: { rejectUnauthorized: true }
 })
 client.connect()
+
+// listen on all watchers
+const query = client.query('LISTEN watchers')
 
 // when a notification comes up show in console
 client.on('notification', (msg) => {
@@ -22,21 +39,33 @@ client.on('notification', (msg) => {
     io.emit('message', msg.payload);
 });
 
-// listen on all watchers
-const query = client.query('LISTEN watchers')
-
 // when we get a request on / send the index.html page
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    if (req.session.user) {
+        // user is logged in
+        res.sendFile(__dirname + '/public/answer.html');
+    } else {
+        // not logged in
+        res.sendFile(__dirname + '/public/login.html');
+    }
 });
+
+app.post('/', (req, res) => {
+    console.log('login', req)
+});
+
+// other static files are served from the public folder
+app.use(express.static(path.join(__dirname, '/public')));
+
 
 // when a new client connects to the socket
 io.on('connection', (socket) => {
     socket.on('message', (msg) => {
-        const query = client.query('insert into foo (name)values($1)', [msg])
+        const query = client.query('insert into foo (name) values ($1)', [msg])
   });
 });
 
+// listen on the port, by default 3000
 http.listen(port, () => {
   console.log('listening on *:' + port);
 });
